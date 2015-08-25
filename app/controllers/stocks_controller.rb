6a30ca4d03188fe416d33c2f771ac17784577a26
adit <adi.tiatama@dwp.co.id>
2015-08-26 00:13:00 +0700

    stock order by kode barang

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,11 @@ class StocksController < ApplicationController
   load_and_authorize_resource
   def index
     query = params[:name] ? params[:name].downcase : ""
-    @stocks = Stock.joins(:barang).where("(lower(barangs.code) like '%#{query}%' or lower(barangs.name) like '%#{query}%')").page(params[:page])
+    @stocks = Stock.joins(:barang)
+                   .where("(lower(barangs.code) like '%#{query}%' OR
+                            lower(barangs.name) like '%#{query}%')")
+                   .order("barangs.code ASC")
+                   .page(params[:page])
   end
 
   def show
